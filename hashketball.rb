7a085ab4f8c27a6d60f9f8dd9bb21c4d6c61a024
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 22:57:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ end
 def num_points_scored(player_search)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if player[:player_name] 
+      if player[:player_name] == player_search
     end
   end
 end
