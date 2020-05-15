ca1103cd641135244543e06eb9ea6c45356bc86d
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 23:05:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,6 +170,7 @@ def player_numbers(number_input)
       team_info.each do |key, value|
         if key == :players
           value.each do |player|
+          end
         end
       end
     end
